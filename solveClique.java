e57e912928dec69d58b28cf8673696c87889913b
kesher1 <kesher1@ash.ad.ilstu.edu>
2020-03-03 21:19:57 -0600

    restructured methods

--- solveClique.java
+++ solveClique.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 public class solveClique
 {
+  static String inputFile;
   static int[] degree;
   static int[][] edges;
   static int graphSize;
@@ -19,11 +20,13 @@ public class solveClique
   static long timeLimit = -1;
   static int maxK;
   static int[] maxClique;
+  static int graphCount;
+  static int totalEdges;
 
   public static void main(String[] args)
   {
     //Gather input from args
-    String inputFile = args[0];
+    inputFile = args[0];
 
     Scanner scan = null;
     try
@@ -35,16 +38,14 @@ public class solveClique
       System.out.println("Unable to open file " + inputFile);
     }
 
-    System.out.println("* Max Cliques in " + inputFile);
-    System.out.println("   (|V|,|E|) Cliques (size, ms used)");
+    printHeader();
 
     graphSize = 0;
-    int graphCount = 0;
+    graphCount = 0;
 
     String readLine = "";
     Scanner readTokens;
     int temp;
-    int totalEdges;
 
     //Loop to read graphs from file
     while (scan.hasNext())
@@ -92,44 +93,30 @@ public class solveClique
       //account for double-counted (symetrical) edges
       totalEdges = totalEdges / 2;
 
-      System.out.print("G" + graphCount
-        + " (" + graphSize + ", " + totalEdges + ") ");
+      findMaxClique();
 
-      //begin timer
-      cpuTime = System.currentTimeMillis();
-      //Create two lists for vertices
-      ArrayList<Integer> currentClique = new ArrayList<Integer>();
-      ArrayList<Integer> toCheck = new ArrayList<Integer>(graphSize);
-      //add all vertices to unchecked list
-      for (int i=0; i<graphSize; i++)
-      {
-        toCheck.add(i);
-      }
-      //recursively find largest clique
-      increaseClique(currentClique,toCheck);
+      printMaxClique();
 
-      //Output found clique data
-      System.out.print("{");
-      int counter = 0;
-      //Print vertices from list contained in clique
-      for (int i=0; i < graphSize ; i++)
-      {
-        if (maxClique[i] == 1)
-        {
-          System.out.print(i);
-          //Output commas after all but final value
-          if (counter < (maxK - 1))
-          {
-            System.out.print(", ");
-            counter++;
-          }
-        }
-      }
-      System.out.print("} (size=" + maxK + ", ");
-      System.out.println((System.currentTimeMillis() - cpuTime) + " ms)");
     }
+
     //Close scanner
     scan.close();
+  }
+
+  public static void findMaxClique()
+  {
+    //begin timer
+    cpuTime = System.currentTimeMillis();
+    //Create two lists for vertices
+    ArrayList<Integer> currentClique = new ArrayList<Integer>();
+    ArrayList<Integer> toCheck = new ArrayList<Integer>(graphSize);
+    //add all vertices to unchecked list
+    for (int i=0; i<graphSize; i++)
+    {
+      toCheck.add(i);
+    }
+    //recursively find largest clique
+    increaseClique(currentClique,toCheck);
   }
 
   public static void increaseClique(ArrayList<Integer> currentClique, ArrayList<Integer> toCheck)
@@ -187,6 +174,38 @@ public class solveClique
       maxClique[i] = 1;
       maxK = currentClique.size();
     }
+  }
+
+  public static void printHeader()
+  {
+    System.out.println("* Max Cliques in " + inputFile);
+    System.out.println("   (|V|,|E|) Cliques (size, ms used)");
+  }
+
+  public static void printMaxClique()
+  {
+    System.out.print("G" + graphCount
+      + " (" + graphSize + ", " + totalEdges + ") ");
+
+    //Output found clique data
+    System.out.print("{");
+    int counter = 0;
+    //Print vertices from list contained in clique
+    for (int i=0; i < graphSize ; i++)
+    {
+      if (maxClique[i] == 1)
+      {
+        System.out.print(i);
+        //Output commas after all but final value
+        if (counter < (maxK - 1))
+        {
+          System.out.print(", ");
+          counter++;
+        }
+      }
+    }
+    System.out.print("} (size=" + maxK + ", ");
+    System.out.println((System.currentTimeMillis() - cpuTime) + " ms)");
   }
 
 }
